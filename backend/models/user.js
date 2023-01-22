dc69bcc76ca788a63bd6ce2d055ee44a269bee87
JulieBodette <jr3python@gmail.com>
2023-01-21 22:35:07 -0500

    Added email and password to user schema. still need to update the createuser conroller so the info actually gets stored in these fields.

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,23 +2,20 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
-  name:
-  {
+  name: {
     type: String,
     required: true,
     minlength: 2,
     maxlength: 30,
   },
 
-  about:
-  {
+  about: {
     type: String,
     required: true,
     minlength: 2,
     maxlength: 30,
   },
-  avatar:
-  {
+  avatar: {
     type: String,
     required: true,
     validate: {
@@ -27,7 +24,19 @@ const userSchema = new mongoose.Schema({
       },
     },
   },
-
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+    },
+  },
 });
 
 module.exports = mongoose.model('user', userSchema);
