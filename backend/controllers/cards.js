00f2386ec092b5e68a167edcb022f9c654175ea9
JulieBodette <jr3python@gmail.com>
2023-03-05 16:26:26 -0500

    added errors to cards.js

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -1,26 +1,27 @@
 const Card = require('../models/card');
 const { NOT_FOUND, SERVER_ERROR, INVALID_INPUT } = require('../utils');
+const { InvalidInput, NotFound, ServerError } = require('../errors');
 
 // getCards returns all cards
-const getCards = (req, res) => {
+const getCards = (req, res, next) => {
   Card.find({})
     .then((cards) => res.send({ data: cards })) // returns to the client all the cards
-    .catch((err) => res.status(SERVER_ERROR).send({ message: err.message }));
+    .catch((err) => next(new ServerError(err.message)));
   // err is an object so we use err.message to get the message string
 };
 
 // createCard creates a new card
-const createCard = (req, res) => {
+const createCard = (req, res, next) => {
   const owner = req.user._id;
   const { name, link } = req.body; // get name etc out of the request body
   Card.create({ name, link, owner })
     .then((card) => res.send(card)) // returns to the client the user they just created
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(INVALID_INPUT).send({ message: err.message });
+        next(new InvalidInput(err.message));
         // error status 400 because user sent invalid input
       } else {
-        res.status(SERVER_ERROR).send({ message: err.message });
+        next(new ServerError(err.message));
         // error status 500-server error
       }
     });
@@ -28,7 +29,7 @@ const createCard = (req, res) => {
 };
 
 // deleteCard deletes a card by ID
-const deleteCard = (req, res) => {
+const deleteCard = (req, res, next) => {
   //check that the card belongs to the current user (otherwise they do not have permission to delete it)
 
   //req.user._id is the id of the current user/the user sending the request
@@ -46,16 +47,17 @@ const deleteCard = (req, res) => {
     .then((card) => res.send({ data: card })) // returns to the client the card they just deleted
     .catch((err) => {
       if (err.message == 'Forbidden') {
-        res.status(401).send({
-          message:
-            'That card does not belong to that user. They are not authorized to delete it.',
-        });
+        next(
+          new NotAuthorized(
+            'That card does not belong to that user. They are not authorized to delete it.'
+          )
+        );
       } else if (err.name === 'CastError') {
-        res.status(INVALID_INPUT).send({ message: 'Invalid card ID' });
+        next(new InvalidInput('Invalid card ID'));
       } else if (err.name === 'DocumentNotFoundError') {
-        res.status(NOT_FOUND).send({ message: 'That card ID does not exist' });
+        next(new NotFound('That card ID does not exist'));
       } else {
-        res.status(SERVER_ERROR).send({ message: err.message });
+        next(new ServerError(err.message));
       }
     });
   // err is an object so we use err.message to get the message string
@@ -63,7 +65,7 @@ const deleteCard = (req, res) => {
 
 // likeCard
 // PUT /cards/:cardId/likes — like a card
-const likeCard = (req, res) => {
+const likeCard = (req, res, next) => {
   // special operation $addToSet means we only add the like if it is not already there
   // ie each user can only like a card once
   Card.findByIdAndUpdate(
@@ -78,18 +80,18 @@ const likeCard = (req, res) => {
     .then((card) => res.send({ card }))
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(INVALID_INPUT).send({ message: 'Invalid card ID' });
+        next(new InvalidInput('Invalid card ID'));
       } else if (err.name === 'DocumentNotFoundError') {
-        res.status(NOT_FOUND).send({ message: 'That card ID does not exist' });
+        next(new NotFound('That card ID does not exist'));
       } else {
-        res.status(SERVER_ERROR).send({ message: err.message });
+        next(new ServerError(err.message));
       }
     });
   // err is an object so we use err.message to get the message string
 };
 
 // dislikeCard
-const dislikeCard = (req, res) => {
+const dislikeCard = (req, res, next) => {
   // special operation $pull means we remove the item from the array
   Card.findByIdAndUpdate(
     req.params.cardId,
@@ -101,11 +103,11 @@ const dislikeCard = (req, res) => {
     .then((card) => res.send({ card }))
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(INVALID_INPUT).send({ message: 'Invalid card ID' });
+        next(new InvalidInput('Invalid card ID'));
       } else if (err.name === 'DocumentNotFoundError') {
-        res.status(NOT_FOUND).send({ message: 'That card ID does not exist' });
+        next(new NotFound('That card ID does not exist'));
       } else {
-        res.status(SERVER_ERROR).send({ message: err.message });
+        next(new ServerError(err.message));
       }
     });
 };
