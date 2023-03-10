5047ea717b017cb217c6230bcd13e16467367a34
JulieBodette <jr3python@gmail.com>
2023-03-10 00:43:36 -0500

    removed comments and the word Hello

--- backend/middleware/validation.js
+++ backend/middleware/validation.js
@@ -11,30 +11,19 @@ const validateURL = (value, helpers) => {
 //module.exports.ValidateUser = celebrate({});
 
 module.exports.ValidateCard = celebrate({
-  //TO DO: added Hello to the beginning of each message for testing purposes- delete later
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30).messages({
-      'string.min': 'Hello The minimum length of the "name" field is 2',
-      'string.max': 'HelloThe maximum length of the "name" field is 30',
-      'string.empty': 'HelloThe "name" field must be filled in',
+      'string.min': 'The minimum length of the "name" field is 2',
+      'string.max': 'The maximum length of the "name" field is 30',
+      'string.empty': 'The "name" field must be filled in',
     }),
 
     link: Joi.string().required().custom(validateURL).messages({
-      'string.empty': 'Hello The "link" field must be filled in',
-      'string.uri': 'Hello the "link" field must be a valid url',
+      'string.empty': 'The "link" field must be filled in',
+      'string.uri': 'The "link" field must be a valid url',
     }),
     likes: Joi.array().required(),
     owner: Joi.object().required(),
     createdAt: Joi.date(),
-
-    //TO DO: ADD THIS
-    //likes is an array and its required
-    //owner is a string and its required
   }),
 });
-
-//card has this
-// name: imageName,
-// link: imageLink,
-// likes: [], //starts with no likes
-// owner: currentUser,
