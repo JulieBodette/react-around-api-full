3f428e63f51e1d2999ce7f8aa23660d0a3b5ae4e
JulieBodette <jr3python@gmail.com>
2023-03-11 13:50:48 -0500

    fixed error with server crashing when user entered incorrect username/password
    there are multiple ways to do this:
    - throw Promise.reject(new Error('Incorrect password or email')); is INCORRECT, crashes server
    - return Promise.reject(new Error('Incorrect password or email')); is fine
    - throw new Error('Incorrect password or email'); is fine (this is what I did)

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -17,7 +17,7 @@ const login = (req, res, next) => {
       if (!user) {
         // user not found
         // fire the catch block with an error
-        throw Promise.reject(new Error('Incorrect password or email'));
+        throw new Error('Incorrect password or email');
       }
       // user found - check if the password is correct
 
@@ -26,7 +26,7 @@ const login = (req, res, next) => {
       // The email and password are correct.
       // create a JSON web token (JWT) that expires after a week.
       if (!isPasswordCorrect) {
-        throw Promise.reject(new Error('Incorrect password or email'));
+        throw new Error('Incorrect password or email');
         // If the password is incorrect, fire the catch block with an error
       }
       // if we got here, password is correct
@@ -46,18 +46,18 @@ const login = (req, res, next) => {
 };
 
 const createUser = (req, res, next) => {
-  const {
-    name, about, avatar, email, password,
-  } = req.body; // get name etc out of the request body
+  const { name, about, avatar, email, password } = req.body; // get name etc out of the request body
   bcryptjs
     .hash(password, 10)
-    .then((hash) => User.create({
-      name,
-      about,
-      avatar,
-      email,
-      password: hash,
-    }))
+    .then((hash) =>
+      User.create({
+        name,
+        about,
+        avatar,
+        email,
+        password: hash,
+      })
+    )
     .then((user) => res.send({ user })) // returns to the client the user they just created
     .catch((err) => {
       if (err.name === 'ValidationError') {
@@ -122,7 +122,7 @@ const updateUserInfo = (req, res, next) => {
       new: true, // the then handler receives the updated entry as input
       runValidators: true, // the data will be validated before the update
       upsert: true, // if the user entry wasn't found, it will be created
-    },
+    }
   )
     .orFail() // throws an error if user does not exist
     .then((user) => {
@@ -142,8 +142,8 @@ const updateUserInfo = (req, res, next) => {
       } else if (err.name === 'ValidationError') {
         next(
           new InvalidInput(
-            'Invalid input. Make sure the about field is minimum 2 and max 30 characters.',
-          ),
+            'Invalid input. Make sure the about field is minimum 2 and max 30 characters.'
+          )
         );
       } else {
         next(new ServerError(err.message));
@@ -160,7 +160,7 @@ const updateUserAvatar = (req, res, next) => {
       new: true, // the then handler receives the updated entry as input
       runValidators: true, // the data will be validated before the update
       upsert: true, // if the user entry wasn't found, it will be created
-    },
+    }
   )
     .then((user) => {
       // if the json that the client sent does not have an about ie {"avatar":"http://link-to-image"}
@@ -179,8 +179,8 @@ const updateUserAvatar = (req, res, next) => {
       } else if (err.name === 'ValidationError') {
         next(
           new InvalidInput(
-            'Invalid input. Make sure the avatar field is a valid url',
-          ),
+            'Invalid input. Make sure the avatar field is a valid url'
+          )
         );
       } else {
         next(new ServerError(err.message));
