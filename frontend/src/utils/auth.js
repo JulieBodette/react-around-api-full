fc8f2bb432ed867b0d3b9f7f17de4ec072260aa7
JulieBodette <jr3python@gmail.com>
2023-04-13 02:03:11 -0400

    changed baseUrl to have https:// at the beginning in auth

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,6 +1,6 @@
 const BASEURL =
   process.env.NODE_ENV === 'production'
-    ? 'api.juliebook.students.nomoredomainssbs.ru'
+    ? 'http://api.juliebook.students.nomoredomainssbs.ru'
     : 'http://localhost:3000';
 const HEADERS = { 'Content-Type': 'application/json' };
 
